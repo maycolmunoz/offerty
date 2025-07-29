57eddf3a5b1a686caedc247b8e323fc079bec3ef
estivenm0 <estivenyeizon@gmail.com>
2025-07-28 20:55:09 -0500

    refactor: 💡 user factory

--- app-modules/moonlaunch/src/Models/User.php
+++ app-modules/moonlaunch/src/Models/User.php
@@ -3,6 +3,7 @@
 namespace Estivenm0\Moonlaunch\Models;
 
 use App\Models\User as ModelsUser;
+use Estivenm0\Core\Database\Factories\UserFactory;
 use Sweet1s\MoonshineRBAC\Traits\MoonshineRBACHasRoles;
 
 class User extends ModelsUser
@@ -10,4 +11,9 @@ class User extends ModelsUser
     use MoonshineRBACHasRoles;
 
     const SUPER_ADMIN_ROLE_ID = 1;
+
+    protected static function newFactory(): UserFactory
+    {
+        return UserFactory::new();
+    }
 }
