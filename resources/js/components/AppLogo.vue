8cb41be1e3cb615822426aebe48fc0b35efd0223
estivenm0 <estivenyeizon@gmail.com>
2025-08-07 12:08:39 -0500

    refactor: 💡 color and size logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -4,7 +4,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current" />
+        <AppLogoIcon class="size-10" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">OFFERTY</span>
